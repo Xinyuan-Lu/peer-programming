f60dcbc751022cc7fa6e483411f02f00c5c3e038
Xinyuan Lu <xinyuanlu@xinyuans-mbp.home>
2020-05-17 22:43:50 -0700

    auto commit

--- client.cpp
+++ client.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "rpc/client.h"
+#include <string>
 
 int main() {
     // Creating a client that connects to the localhost on port 8080
     rpc::client client("127.0.0.1", 8080);
 
     // Calling a function with paramters and converting the result to int
-    auto result = client.call("add", 2, 3).as<int>();
+    auto result = client.call("add", "a", "b").as<std::string>();
     auto result_1 = client.call("foo");
     std::cout << "The result is: " << result << std::endl;
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
 #include "rpc/server.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+struct product {
+    int weight;
+    std::string edit;
+
+    product(int w, std::string e): weight(w), edit(e){}
+
+    std::string toString() {
+        auto j = json{{"weight", weight}, {"edit", edit}};
+        return j.dump();
+    }
+
+    void fromString(std::string inString){
+        auto j = json::parse(inString);
+        this->weight = j["weight"];
+        this->edit = j["edit"];
+    }
+};
+
 
 void foo() {
     std::cout << "foo was called!" << std::endl;
 }
 
+
+
 int main(int argc, char *argv[]) {
     // Creating a server that listens on port 8080
     rpc::server srv(8080);
+    
+    product test(0, "Hello world");
+    std::string i = test.toString();
+    product nTest(12121, "World Hello");
+    std::cout << nTest.edit << nTest.weight << std::endl;
+    nTest.fromString(i);
+    std::cout << nTest.edit << nTest.weight << std::endl;
 
+    // j[product]
     // Binding the name "foo" to free function foo.
     // note: the signature is automatically captured
     srv.bind("foo", &foo);
 
     // Binding a lambda function to the name "add".
-    srv.bind("add", [](int a, int b) {
-        return a + b;
+    srv.bind("add", [](std::string a, std::string b) {
+        return a+b;
     });
 
     // Run the server loop.
-    srv.run();
+    // srv.run();
 
     return 0;
 }
